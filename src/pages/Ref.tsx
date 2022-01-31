cf147d0ee12ddeb0dfda8556e54f4840de23faab
sgwr-0113 <xxepisode.18xx@gmail.com>
2022-01-31 12:21:57 +0900

    add text

--- src/pages/Ref.tsx
+++ src/pages/Ref.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { RefFocus } from 'components/ref/RefFocus'
+import { RefCompareState } from 'components/ref/RefCompareState'
 
 export const Ref: React.FC = () => {
   return (
     <>
       <h1>useRef</h1>
+      <h2>DOMを参照したい場合</h2>
       <RefFocus />
+      <h2>値を裏で保持したい場合</h2>
+      <RefCompareState />
       <p>
         <Link to="/">トップへ戻る</Link>
       </p>

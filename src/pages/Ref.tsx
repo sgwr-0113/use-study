f7969e9c5e177994b8367dfb7dc11e95372693d6
sgwr-0113 <65204357+sgwr-0113@users.noreply.github.com>
2022-02-01 11:08:14 +0900

    install tailwind

--- src/pages/Ref.tsx
+++ src/pages/Ref.tsx
@@ -6,11 +6,13 @@ import { RefCompareState } from 'components/ref/RefCompareState';
 export const Ref: React.FC = () => {
   return (
     <>
-      <h1>useRef</h1>
-      <h2>DOMを参照したい場合</h2>
-      <RefFocus />
-      <h2>値を裏で保持したい場合</h2>
-      <RefCompareState />
+      <h1 className="text-3xl">useRef</h1>
+      <div className="container border border-red-500 max-w-screen-lg">
+        <h2>DOMを参照したい場合</h2>
+        <RefFocus />
+        <h2>値を裏で保持したい場合</h2>
+        <RefCompareState />
+      </div>
       <p>
         <Link to="/">トップへ戻る</Link>
       </p>

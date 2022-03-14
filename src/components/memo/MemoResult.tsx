fabf6d6e2e695c8070394db79120620fd3bf39a6
sgwr-0113 <xxepisode.18xx@gmail.com>
2022-03-14 13:53:05 +0900

    fix import

--- src/components/memo/MemoResult.tsx
+++ src/components/memo/MemoResult.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useMemo, useRef, useEffect } from 'react';
+import React, { useState, useMemo } from 'react';
 import Button from '@mui/material/Button';
 import { heavyFunc } from 'utils/heavyFunc';
 
